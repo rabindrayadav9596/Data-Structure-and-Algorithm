bba859e6e192e3033331cf9410356ecd913e12da
Rabindra Yadav <rabindrayadav9596@gmail.com>
2020-06-18 21:45:21 -0500

    queues using stack

---
+++ queuesWithStack.py
@@ -0,0 +1,39 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.inStack = []
+        self.outStack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.inStack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.move()
+        return self.outStack.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        self.move()
+        return self.outStack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return self.inStack == [] and self.outStack == []
+
+    def move(self):
+        if self.outStack == []:
+            while self.inStack:
+                self.outStack.append(self.inStack.pop())
